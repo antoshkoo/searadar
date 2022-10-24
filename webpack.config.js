d39663276139f89a4ab11dc7404f2a3118d726e9
Anton Yashin <antoshkoo@gmail.com>
2022-10-24 14:59:06 +0500

    init: webpack, bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+// miniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/main.js",
+  // plugins: [new miniCssExtractPlugin()],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [require("autoprefixer")],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
